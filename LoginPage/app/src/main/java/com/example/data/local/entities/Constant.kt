223745dcd0177df77c3bb70c8e3bd282c6b52232
Godwin Bime <tardzenyuy1989@gmail.com>
2024-12-14 22:39:04 -0500

    Ignore Constant.kt in .gitignore

--- LoginPage/app/src/main/java/com/example/data/local/entities/Constant.kt
+++
@@ -1,6 +0,0 @@
-package com.example.data.local.entities
-
-// web client ID, Needed in google sign in component file
-object Constant {
-    const val SERVERCLIENT = "92085632653-eceg34oh8h5mobtk7nrqlspcall0cb65.apps.googleusercontent.com"
-}
